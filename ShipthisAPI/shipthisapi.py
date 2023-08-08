c7e9343aae0a8b29213b905877e783ef568e5566
TC Gopal <tcgopal@shipthis.co>
2023-08-08 17:16:32 +0530

    checked for the error raised by the client

--- ShipthisAPI/shipthisapi.py
+++ ShipthisAPI/shipthisapi.py
@@ -22,11 +22,17 @@ class ShipthisAPI:
             "user_type": self.user_type,
             "location": 'new_york'
         }
-        resp = requests.request(method, self.base_api_endpoint + path, data=request_data or {}, headers=headers)
-        if resp.status_code == 200:
-            raw_response = resp.json()
-            if raw_response.get("success"):
-                return raw_response.get("data")
+        fetched_response = requests.request(method, self.base_api_endpoint + path, data=request_data or {}, headers=headers, params=query_params)
+        result = fetched_response.json()
+        
+        if fetched_response.status_code == 200:
+            if result.get("success"):
+                return result.get("data")
+            else:
+                error_message = result.get("errors") or "API call failed. Please check your internet connection or try again later"
+                return error_message[0].get('message') if error_message[0].get('message') else "Please provide the necessary requirements or try again later"
+        else:
+            return "Internal Server error, please try again later"
 
     def info(self) ->  Dict:
         info_resp = self._make_request('GET', 'auth/info')
@@ -36,27 +42,43 @@ class ShipthisAPI:
 
     def get_one_item(self, collection_name: str, params=None) ->  Dict:
         resp = self._make_request('GET', 'incollection/' + collection_name)
-        if resp.get("items"):
-            # return first elem
-            return resp.get("items")[0]
+        if  isinstance(resp, dict):
+            if resp.get("items"):
+                # return first elem
+                return resp.get("items")[0]
+        else:
+            return resp
 
-    def get_list(self, collection_name: str, params=None) -> List[Dict]:
-        resp = self._make_request('GET', 'incollection/' + collection_name)
-        if resp.get("items"):
-            return resp.get("items", [])
-        
+    def get_list(self, collection_name: str, params=None) -> List[Dict] or str:
+        get_list_response = self._make_request('GET', 'incollection/' + collection_name, params)
+        if isinstance(get_list_response, str):
+            return get_list_response
+        else:
+            if get_list_response.get("items", False):
+                return get_list_response.get("items", [])
+            else:
+                return get_list_response
 
 
     def create_item(self, collection_name: str, data=None) ->  Dict:
         resp = self._make_request('POST', 'incollection/' + collection_name, request_data={"reqbody": data})
-        if resp.get("data"):
-            return resp.get("data")
+        if  isinstance(resp, dict):
+            if resp.get("data"):
+                return resp.get("data")
+        else:
+            return resp
 
     def update_item(self, collection_name: str, object_id: str, updated_data=None) ->  Dict:
         resp = self._make_request('PUT', 'incollection/' + collection_name + '/' + object_id, request_data={"reqbody": updated_data})
-        if resp.get("data"):
-            return resp.get("data")
+        if  isinstance(resp, dict):
+            if resp.get("data"):
+                return resp.get("data")
+        else:
+            return resp
 
     def delete_item(self, collection_name: str, object_id: str) -> Dict:
         resp = self._make_request('DELETE', 'incollection/' + collection_name + '/' + object_id)
+        # if  isinstance(resp, str):
+        #     return resp
+        # else:
         return resp
